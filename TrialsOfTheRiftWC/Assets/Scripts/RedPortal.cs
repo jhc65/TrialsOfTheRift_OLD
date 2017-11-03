07c9f1b44b37e5a584dd73be64b6ea380dc8839c
Joe <joseph@rkiapps.com>
2017-11-03 00:24:35 -0400

    - add portals

---
+++ TrialsOfTheRiftWC/Assets/Scripts/RedPortal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedPortal : MonoBehaviour {
+
+    public Vector3 v_playerSpawnPosition = new Vector3(13.75f, 0.0f, -13.27f);
+    public Vector3 v_shotSpawnPosition = new Vector3(12.0f, 0.0f, -13.27f);
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag == "Player") {
+            PlayerDidCollide(collision.collider.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Shot") {
+            ShotDidCollide(other.gameObject);
+        }
+    }
+
+    public void PlayerDidCollide(GameObject player) {
+        player.transform.position = v_playerSpawnPosition;
+    }
+
+    private void ShotDidCollide(GameObject shot) {
+        shot.transform.position = v_playerSpawnPosition;
+    }
+}
