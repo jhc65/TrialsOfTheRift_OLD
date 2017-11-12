974850cf4d91196a0a4e46ac290a89bcd4e90d42
SlightPlus <nero.DDX@gmail.com>
2017-11-12 00:00:14 -0500

    Fixed merge issues. [Enemies + UI + Spell + PlayerDeath]

--- TrialsOfTheRiftWC/Assets/Scripts/IceController.cs
+++ TrialsOfTheRiftWC/Assets/Scripts/IceController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class IceController : SpellController {
+    private float f_iceDamage = Constants.SpellStats.C_IceDamage;
 
-	// SendMessage() calls all functions named the parameter that exist in MonoBehavior 
-	// scripts on the GameObject. This way, we don't have worry about differentiating 
-	// between freezing a player or freezing an enemy. It'll just find the function
-	// named Freeze() on the GameObject's controller script.
-	protected override void ApplyEffect(GameObject go_target){
+    // SendMessage() calls all functions named the parameter that exist in MonoBehavior 
+    // scripts on the GameObject. This way, we don't have worry about differentiating 
+    // between freezing a player or freezing an enemy. It'll just find the function
+    // named Freeze() on the GameObject's controller script.
+    protected override void ApplyEffect(GameObject go_target){
+        go_target.GetComponent<PlayerController>().DoDamage(f_iceDamage);
 		go_target.SendMessage("Freeze"); 
 	}
+
+    protected override void BuffSpell()
+    {
+        f_iceDamage = f_iceDamage * Constants.SpellStats.C_IceDamageMultiplier;
+        transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
+    }
 }
--- TrialsOfTheRiftWC/Assets/Scripts/PlayerController.cs
+++ TrialsOfTheRiftWC/Assets/Scripts/PlayerController.cs
@@ -15,14 +15,19 @@ public class PlayerController : MonoBehaviour{
 	public bool b_canMove;					// identifies if the player is frozen
 	public GameObject go_windShot;			// wind spell object
 	public GameObject go_iceShot;			// ice spell object
+
+    //In Constants
 	public float f_spellSpeed;				// spell movement speed
     public float f_windSpeed;               // wind spell movement speed
     public float f_iceSpeed;                // ice spell movement speed
 	public float f_windRecharge;			// delay between wind spells
 	public float f_iceRecharge;             // delay between ice spells
+    public GameObject c_playerCapsule;     // Player main body
+    public GameObject c_playerWisp;        // Player wisp body
 
-	private float f_nextWind;				// time next wind spell can be cast
+    private float f_nextWind;				// time next wind spell can be cast
 	private float f_nextIce;				// time next ice spell can be cast
+    private float f_playerHealth;           // Players current health value
 
 
 	private void Move(){
@@ -72,12 +77,37 @@ public class PlayerController : MonoBehaviour{
 		}
 	}
 
+    public void DoDamage(float damageIn){
+        f_playerHealth -= damageIn;
+    }
+
 	public Constants.Color GetColor(){
 		return e_Color;
 	}
 
+    private void PlayerDeath() {
+        f_nextWind = Time.time + (Constants.PlayerStats.C_RespawnTimer + 3.0f);
+        f_nextIce = Time.time + (Constants.PlayerStats.C_RespawnTimer + 3.0f);
+        c_playerCapsule.SetActive(false);
+        Drop();
+        c_playerWisp.SetActive(true);
+        i_moveSpeed = Constants.PlayerStats.C_WispMovementSpeed;
+        Invoke("PlayerRespawn", Constants.PlayerStats.C_RespawnTimer);
+    }
+
+    private void PlayerRespawn() {
+        c_playerCapsule.SetActive(true);
+        c_playerWisp.SetActive(false);
+        f_playerHealth = Constants.PlayerStats.C_MaxHealth;
+        i_moveSpeed = Constants.PlayerStats.C_MovementSpeed;
+        f_nextWind = Time.time;
+        f_nextIce = Time.time;
+    }
+
 	private void Start(){
+        f_playerHealth = Constants.PlayerStats.C_MaxHealth;
 		b_canMove = true;
+        i_moveSpeed = Constants.PlayerStats.C_MovementSpeed;
 		f_nextWind = 0;
 		f_nextIce = 0;
 		if (transform.position.x > 0)
@@ -89,18 +119,20 @@ public class PlayerController : MonoBehaviour{
 	private void FixedUpdate(){
 		if (b_canMove){
 			Move();
+            f_nextWind += Time.deltaTime;
+            f_nextIce += Time.deltaTime;
 
 			// spells
 			if (!go_flagObj){
-				if (InputManager.GetButton(InputManager.Axes.WINDSPELL, i_playerNumber) && Time.time > f_nextWind){   // checks for fire button and if time delay has passed
-					f_nextWind = Time.time + f_windRecharge;
+				if (InputManager.GetButton(InputManager.Axes.WINDSPELL, i_playerNumber) && f_nextWind > f_windRecharge){   // checks for fire button and if time delay has passed
+					f_nextWind = 0;
 					GameObject go_spell = Instantiate(go_windShot, t_spellSpawn.position, t_spellSpawn.rotation);
 					go_spell.GetComponent<SpellController>().e_color = e_Color;
 					Debug.Log(transform.forward.normalized);
 					go_spell.GetComponent<Rigidbody>().velocity = transform.forward * f_spellSpeed;
 				}
-				if (InputManager.GetButton(InputManager.Axes.ICESPELL, i_playerNumber) && Time.time > f_nextIce){   // checks for fire button and if time delay has passed
-					f_nextIce = Time.time + f_iceRecharge;
+				if (InputManager.GetButton(InputManager.Axes.ICESPELL, i_playerNumber) && f_nextIce > f_iceRecharge){   // checks for fire button and if time delay has passed
+					f_nextIce = 0;
 					GameObject go_spell = Instantiate(go_iceShot, t_spellSpawn.position, t_spellSpawn.rotation);
 					go_spell.GetComponent<SpellController>().e_color = e_Color;
 					go_spell.GetComponent<Rigidbody>().velocity = transform.forward * f_spellSpeed;
@@ -110,6 +142,9 @@ public class PlayerController : MonoBehaviour{
 	}
 
 	private void Update(){
+        if (f_playerHealth <= 0.0f) {
+            PlayerDeath();
+        }
 		if (InputManager.GetButtonDown(InputManager.Axes.INTERACT, i_playerNumber)){
 			if (go_flagObj){
 				Drop();
@@ -129,4 +164,12 @@ public class PlayerController : MonoBehaviour{
 	public void TakeDamage(float damage){
 		print("ow");
 	}
+
+    public float GetNextWind() {
+        return f_nextWind;
+    }
+
+    public float GetNextIce() {
+        return f_nextIce;
+    }
 }
