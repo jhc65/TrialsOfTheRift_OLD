f5555e16dcb2944b71ed6dc7f9588b36d6dee298
Zak-Olyarnik <zwo24@drexel.edu>
2017-12-07 22:53:39 -0500

    controller debugging tests

--- TrialsOfTheRiftWC/Assets/Scripts/PlayerControlsTest.cs
+++ TrialsOfTheRiftWC/Assets/Scripts/PlayerControlsTest.cs
@@ -7,45 +7,72 @@ public class PlayerControlsTest : MonoBehaviour {
 
 	public Text t;
 	public int playerNum;
-	public InputManager.Axes horizontal, vertical, wind, ice, interact, menu, submit, cancel;
+	public InputManager.Axes horizontal, vertical, wind, ice, interact, menu, submit, cancel, electric, mm, aimh, aimv;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        // controller mapping defaults to XBOX, so only change if PS4 controller connected
+        if (Input.GetJoystickNames()[0] == "Wireless Controller")
+        { // PS4 connection message
+            InputManager.P1_Map = InputManager.P1_PS4;
+            Debug.Log("P1 PS4");
+            t.text = "Press X";
+        }
+        else
+        {
+            InputManager.P1_Map = InputManager.P1_XBOX;
+            Debug.Log("P1 XBOX");
+            t.text = "Press A";
+        }
+    }
 
-	// Update is called once per frame
-	//void Update()
-	//{
-	//	float speed = 10.0F;
-	//	float h = speed * InputManager.GetAxis(horizontal);
-	//	float v = speed * InputManager.GetAxis(vertical);
-	//	t.transform.Translate(h, v, 0);
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 10.0F;
+        float h = speed * InputManager.GetAxis(aimh, playerNum);
+        float v = speed * InputManager.GetAxis(aimv, playerNum);
+        t.transform.Translate(h, v, 0);
 
-	//	if (InputManager.GetButtonDown(wind))
-	//	{
-	//		t.text = "P" + playerNum + " wind";
-	//	}
-	//	if (InputManager.GetButtonDown(ice))
-	//	{
-	//		t.text = "P" + playerNum + " ice";
-	//	}
-	//	if (InputManager.GetButtonDown(interact))
-	//	{
-	//		t.text = "P" + playerNum + " interact";
-	//	}
-	//	if (InputManager.GetButtonDown(menu))
-	//	{
-	//		t.text = "P" + playerNum + " menu";
-	//	}
-	//	if (InputManager.GetButtonDown(submit))
-	//	{
-	//		t.text = "P" + playerNum + " submit";
-	//	}
-	//	if (InputManager.GetButtonDown(cancel))
-	//	{
-	//		t.text = "P" + playerNum + " cancel";
-	//	}
-	//}
+        if (InputManager.GetButtonDown(wind, playerNum))
+        {
+            t.text = "P" + playerNum + " wind";
+        }
+        if (InputManager.GetButtonDown(ice, playerNum))
+        {
+            t.text = "P" + playerNum + " ice";
+        }
+        if (InputManager.GetButtonDown(interact, playerNum))
+        {
+            t.text = "P" + playerNum + " interact";
+        }
+        if (InputManager.GetButtonDown(menu, playerNum))
+        {
+            t.text = "P" + playerNum + " menu";
+        }
+        if (InputManager.GetButtonDown(submit, playerNum))
+        {
+            t.text = "P" + playerNum + " submit";
+        }
+        if (InputManager.GetButtonDown(cancel, playerNum))
+        {
+            t.text = "P" + playerNum + " cancel";
+        }
+        if (InputManager.GetButtonDown(electric, playerNum))
+        {
+            t.text = "P" + playerNum + " electric";
+        }
+        if (InputManager.GetButtonDown(mm, playerNum))
+        {
+            t.text = "P" + playerNum + " mm";
+        }
 
+        for (int i = 3; i < 29; i++)
+        {
+            if(Input.GetAxis("Axis" + i) != 0 && Input.GetAxis("Axis" + i) != -1)
+            {
+                Debug.Log("Axis" + i + " - " + Input.GetAxis("Axis" + i));
+            }
+        }
+    }
 }
